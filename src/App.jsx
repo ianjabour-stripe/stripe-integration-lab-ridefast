b141a91d4dfcf62f58ecb3cc7da8d5eaeeae00a1
Ian Jabour <ianjabour@stripe.com>
2026-01-13 17:00:00 -0800

    Add detail view

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,90 @@
+import * as React from 'react';
 import "./App.css";
 import {PRODUCTS} from './products';
 
-const ProductListing = ({product, onClick}) => {
+const ProductImage = ({src, alt, onClick}) => {
+  return (
+    <div className="ProductImage" onClick={onClick}>
+      <figure style={{backgroundImage: `url(${src}`}} alt={alt} />
+    </div>
+  )
+}
+
+const ProductDetail = ({productId, onClosePreview}) => {
+  React.useEffect(() => {
+    document.body.classList.add('bodyLock');
+
+    return () => {
+      document.body.classList.remove('bodyLock');
+    } 
+  }, []);
+
+  React.useEffect(() => {
+    function handleKeydown(e) {
+      if (e.key === 'Escape') {
+        onClosePreview();
+      }
+    }
+    document.addEventListener('keydown', handleKeydown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeydown);
+    } 
+  }, [onClosePreview]);
+
+  const product = React.useMemo(() => PRODUCTS.find(({id}) => id === productId), [productId])
+
+  return product ? (
+    <div className="ProductDetail" onClick={onClosePreview}>
+      <div className="content">
+        <ProductImage src={product.image} alt={product.name} />
+        <p className="description">
+          <h4>{product.name}</h4>
+          {product.description}
+        </p>
+      </div>
+    </div>
+  ) : null
+}
+
+const ProductListing = ({product, onSelect, onPreview}) => {
   return (
     <div className="ProductListing">
-      <div className="image">
-        <figure style={{backgroundImage: `url(${product.image}`}} alt={product.name} />
-      </div>
-      <div className="description">
+      <ProductImage src={product.image} alt={product.name} onClick={() => onPreview(product)} />
+      <div className="info">
         <div>{product.name}</div>
         <div>${product.price} / day</div>
       </div>
-      <button onClick={() => onClick(product)}>Pick this ride</button>
+      <button onClick={() => onSelect(product)}>Pick this ride</button>
     </div>
   )
 }
 
 const App = () => {
-  const handleClick = (product) => {
-    alert(product)
+  const [previewProduct, setPreviewProduct] = React.useState(null);
+
+  const handleOpenPreview = (product) => {
+    setPreviewProduct(product.id)
   }
+
+  const handleSelect = (product) => {
+    alert(`${product.name} was selected`);
+  }
+
+  const handleClosePreview = React.useCallback(() => {
+    setPreviewProduct(null);
+  }, [])
 
   return (
     <div className="App">
       <header>RideFast • Car Rentals</header>
       <main className="ProductListings">
         {PRODUCTS.map((product) => (
-          <ProductListing key={product.id} product={product} onClick={handleClick} />
+          <ProductListing key={product.id} product={product} onSelect={handleSelect} onPreview={handleOpenPreview} />
         ))}
+        {previewProduct ? (
+          <ProductDetail productId={previewProduct} onClosePreview={handleClosePreview} />
+        ) : null}
       </main>
     </div>
   )
